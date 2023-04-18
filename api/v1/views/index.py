736c4f9b1f8a117463c6b556994abd539f591edb
Aminekham <lkham0508@gmail.com>
2023-04-19 00:30:20 +0200

    fixing circular import error

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This is the routing of our API"""
 from flask import jsonify
-from api.v1.app import app
+from api.v1.views import app_views
 
-@app.route('/status')
+@app_views.route('/status')
 def status():
     """Return the status of the API"""
     return jsonify({'status': 'OK'})
