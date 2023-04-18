6d323ae54edcf62a22104c9f198eccd3b068a858
Aminekham <lkham0508@gmail.com>
2023-04-19 00:14:41 +0200

    fixing documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""This module is the main app in our API
+"""
+This module is the main app in our API.
 """
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 
 
-app = Flask(app_views)
+app = Flask(__name__)
+app.register_blueprint(app_views)
 
-@app.teardown_appcontext()
-def close():
+@app.teardown_appcontext
+def close_storage(exception):
+    """Closes the database connection."""
     storage.close()
 
 if __name__ == "__main__":
