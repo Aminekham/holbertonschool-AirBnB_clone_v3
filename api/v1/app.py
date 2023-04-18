f96cd24d84f9571733ac7a9a3120df336920aead
Aminekham <lkham0508@gmail.com>
2023-04-19 00:17:17 +0200

    fixing documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 """
 This module is the main app in our API.
 """
+
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -10,10 +11,15 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def close_storage(exception):
-    """Closes the database connection."""
+def teardown_storage():
+    """
+    Function to be called when the Flask app context is torn down.
+    This function closes the storage engine to prevent resource leaks.
+    """
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", threaded=True)
