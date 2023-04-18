8d10ad3dff4c6f250e6d167ce6f51d01bbf63178
Aminekham <lkham0508@gmail.com>
2023-04-19 00:36:34 +0200

    fixing circular import error

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,4 +6,4 @@ from api.v1.views import app_views
 @app_views.route('/status')
 def status():
     """Return the status of the API"""
-    return jsonify({'status': 'OK'})
+    return ({'status': 'OK'})
