dca1b28becac1a2a5e1145a1e0c51e1f834fcce1
Aminekham <lkham0508@gmail.com>
2023-04-21 04:16:23 +0200

    fixing

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 
+
 @app_views.route('/status')
 def status():
     """Return the status of the API"""
