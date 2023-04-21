0456afaee8bff61dcb8a93e8bf2a2944b1e8dcd9
Aminekham <lkham0508@gmail.com>
2023-04-21 04:26:18 +0200

    fixing

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
-"""
-This module is the main app in our API.
-"""
-
-from flask import Flask, jsonify
-from models import storage
+"""This is the main module that defines the Flask app instance"""
+from flask import Flask, jsonify, make_response, current_app
+from os import environ
 from api.v1.views import app_views
-from flask_cors import CORS
-import os
+from models import storage
+
 
 app = Flask(__name__)
 
 
+@app_views.route('/status')
+def status():
+    """Return the status of the API"""
+    return jsonify({'status': 'OK'})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handles 404 errors"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """ Function to be called when the Flask app context is torn down. 
-    This function closes the storage engine to prevent resource leaks. 
-    """
+def close_db(error):
+    """Closes the database again at the end of the request"""
     storage.close()
-
 
 
 if __name__ == "__main__":
     app.register_blueprint(app_views, url_prefix='/api/v1')
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = os.getenv("HBNB_API_PORT", 5000)
+
+    host = environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = environ.get('HBNB_API_PORT', 5000)
     app.run(host=host, port=port, threaded=True)
