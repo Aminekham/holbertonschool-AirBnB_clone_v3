5d723fd55957b9ad3477857261ce8af4c6e7a730
Aminekham <lkham0508@gmail.com>
2023-04-21 04:00:36 +0200

    index fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,20 +9,3 @@ def status():
     """Return the status of the API"""
     return(jsonify({"status": "OK"}))
 
-
-@app_views.route('/api/v1/stats')
-def get_stats():
-    """Retrieves the number of objects by type"""
-    amenities_count = storage.count("Amenity")
-    cities_count = storage.count("City")
-    places_count = storage.count("Place")
-    reviews_count = storage.count("Review")
-    states_count = storage.count("State")
-    users_count = storage.count("User")
-    stats = {"amenities": amenities_count,
-             "cities": cities_count,
-             "places": places_count,
-             "reviews": reviews_count,
-             "states": states_count,
-             "users": users_count}
-    return(jsonify(stats))
